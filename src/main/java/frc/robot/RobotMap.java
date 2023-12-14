f07ff78207a0acac11d4248ccac7ebd1287f6d6d
Macho-Cat <muadh.ghori@student.tdsb.on.ca>
2023-12-14 17:29:17 -0500

    inital commit

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,23 @@
+package frc.robot;
+
+// import com.revrobotics.CANSparkMax;
+
+public class RobotMap {
+    // ids for base motors
+    public static int leftFrontSpin = 4;
+    public static int leftBackSpin = 5;
+    public static int leftMiddleSpin = 6;
+
+    public static int rightFrontSpin = 7;
+    public static int rightBackSpin = 8;
+    public static int rightMiddleSpin = 9;
+
+    public static int leftFrontDrive = 4;
+    public static int leftBackDrive = 5;
+    public static int leftMiddle = 6;
+
+    public static int rightFrontDrive = 7;
+    public static int rightBackDrive = 8;
+    public static int rightMiddleDrive = 9;
+
+}
